72950d22ae93cebb6241d99eddae2709d2e7f7e2
Scott Simpson <>
2018-02-18 12:34:09 -0800

    updated readme

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              One of three columns
+            </div>
+            <div className="col-sm">
+              One of three columns
+            </div>
+            <div className="col-sm">
+              One of three columns
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
